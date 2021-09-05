01c968b2ad7f643f64e74670c7703ce9450510f8
FellipeToledo <fellipetoledo.dev@gmail.com>
2021-09-05 16:35:00 -0300

    initial commit

---
+++ src/test/java/com/example/datatransferobjectexample/DataTransferObjectExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datatransferobjectexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataTransferObjectExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
